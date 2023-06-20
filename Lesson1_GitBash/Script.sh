6debaf23c0683c4d4b0c93549913b7d99688c751
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-06-20 17:19:08 +0300

    added Lesson1

---
+++ Lesson1_GitBash/Script.sh
@@ -0,0 +1,20 @@
+# Путь в папку выполнения
+cd /d/Courses35/Lesson1_GitBash/script
+
+# Создать 3 папки
+mkdir test1 test2 test3
+
+# Зайти в одну из папок
+cd test1 
+
+# Создать 5 файлов (3 текстовых и 2 json)
+touch file1.txt file2.txt file3.txt file4.json file5.json
+
+# Создать 3 папки
+mkdir p1 p2 p3
+
+# Вывести список содержимого
+ls
+
+# Переместить 2 файла в другую папку
+mv file2.txt file3.txt /d/Courses35/Lesson1_GitBash//script/test2
